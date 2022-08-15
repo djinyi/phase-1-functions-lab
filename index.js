904d49820c9cedd663b30a61f85f6f28de556ec6
Daisy Yi <daisyyi@rocketmail.com>
2022-08-14 21:36:09 -0700

    commit tree 550ceab02798796c43a140fe805885b46d00568b

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+let start;
+
+function distanceFromHqInBlocks(start, destination=42) {
+    if (start>42){
+        return distanceFromHqInBlocks =(start-destination)
+    }
+    else if (start<42) {
+        return distanceFromHqInBlocks =(destination-start)
+    }
+};
+
+function distanceFromHqInFeet() {
+    return distanceFromHqInFeet =(distanceFromHqInBlocks*264);
+}
+
+function calculatesFarePrice() {
+    if((distanceFromHqInBlocks-400)<=2000) {
+        return distanceFromHqInBlocks*.02
+    } else if ((distanceFromHqInBlocks-400)>=200) {
+        return 2000*.02
+    } else if ((distanceFromHqInBlocks-400)>2500) {
+        return 'cannot travel that far'
+    };
+};
+
+
